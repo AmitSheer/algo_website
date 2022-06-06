3b57de7a607dccd607ab5549bc0cf5f7e15abf2d
AmitSheer <amitcohen95@gmail.com>
2022-06-06 22:57:31 +0300

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,66 @@
+import os
+
+from flask import render_template, url_for, redirect, request
+from hypergz import force_directed_hyper_graphs_using_social_and_gravity_scaling, hypergraph, hyperedge, \
+    complete_algorithm, cycle_algorithm, star_algorithm, wheel_algorithm
+import networkx as nx
+from . import app
+from .forms import Parameters
+from matplotlib.figure import Figure
+
+
+
+def random_hypergraph(num_of_vtx, num_of_edges):
+    import random
+    vtx = list(range(num_of_vtx))
+    edges = []
+    for edge in range(num_of_edges):
+        v = set()
+        num_of_vtx_in_edge = random.randint(1, num_of_vtx)
+        for _ in range(num_of_vtx_in_edge):
+            rand_vtx = random.randint(0, num_of_vtx - 1)
+            v.add(rand_vtx)
+        E = hyperedge(list(v))
+        edges.append(E)
+    return hypergraph(vtx, edges)
+
+
+def find_centrality(cent):
+    if cent == 'cl':
+        return nx.closeness_centrality
+    if cent == 'bt':
+        return nx.betweenness_centrality
+    if cent == 'dg':
+        return nx.degree_centrality
+
+
+def find_algo(algo):
+    if algo == 'Comp':
+        return complete_algorithm
+    if algo == 'Cyc':
+        return cycle_algorithm
+    if algo == 'Str':
+        return star_algorithm
+    if algo == 'Wh':
+        return wheel_algorithm
+
+
+@app.route("/result")
+def result():
+    return render_template('resultpage.html')
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    form = Parameters()
+    if request.method == 'POST':
+        fig: Figure = force_directed_hyper_graphs_using_social_and_gravity_scaling(
+            G=random_hypergraph(form.vtx.data, form.edges.data),
+            iterations=form.iter.data,
+            centrality=find_centrality(form.centrality.data),
+            graph_type=find_algo(form.type.data), gravity=form.gravity.data, fig=True)
+        pic_path = os.path.join(app.root_path, 'static\images\plot.png')
+        fig.savefig(pic_path)
+        return redirect(url_for('result'))
+    else:
+        return render_template('homepage.html', form=form)
